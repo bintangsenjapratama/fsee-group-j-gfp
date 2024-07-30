f87bc01e18a2dda644c50cbbb1eb81f89249e9be
LightKazuto <ilhamsidi62@gmail.com>
2024-07-31 06:57:03 +0700

    update: add get all  user end point

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from flask_jwt_extended import create_access_token
+from sqlalchemy import select
 
 from connectors.mysql_connectors import connection
 from models.user import User
@@ -10,6 +11,38 @@ from models.user import User
 users_routes = Blueprint("users_routes", __name__)
 Session = sessionmaker(connection)
 s = Session()
+
+
+@users_routes.route("/getalldata/user", methods=["GET"])
+def get_allUser():
+    try:
+        Session = sessionmaker(bind=connection)
+        with Session() as s:
+            user_query = select(User)
+
+            search_keyword = request.args.get("query")
+            if search_keyword is not None:
+                user_query = user_query.where(User.username.like(f"%{search_keyword}%"))
+
+            result = s.execute(user_query)
+            users = []
+
+            for row in result.scalars():
+                users.append(
+                    {
+                        "id": row.id,
+                        "email": row.email,
+                        "username": row.username,
+                        "role": row.role,
+                    }
+                )
+            return (
+                {"users": users},
+                200,
+            )
+    except Exception as e:
+        print(e)
+        return {"message": "Unexpected Error"}, 500
 
 
 @users_routes.route("/users/register", methods=["POST"])
