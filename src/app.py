2bfb3bac968219180a9d71070042684bae89ff2f
LightKazuto <ilhamsidi62@gmail.com>
2024-07-31 05:58:44 +0700

    update: add add load_dotenv

--- src/app.py
+++ src/app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+
+from connectors.mysql_connectors import connection
+from controllers.users import users_routes
+from models.user import User
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+Session = sessionmaker(connection)
+
+app.register_blueprint(users_routes)
 
 
 @app.route("/")
