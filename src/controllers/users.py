4bb2b239a06ed3ad1ef2c400c7bf56eeb43557d8
LightKazuto <ilhamsidi62@gmail.com>
2024-07-31 06:32:13 +0700

    update: add jwt login, and login end point

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+from flask_jwt_extended import create_access_token
 
 from connectors.mysql_connectors import connection
 from models.user import User
@@ -28,3 +29,26 @@ def register_usersData():
         s.rollback()
         return {"message": "Fail to Register New User"}, 500
     return {"message": "Success to Create New User"}, 200
+
+
+@users_routes.route("/users/login", methods=["POST"])
+def login_userData():
+    try:
+        email = request.form["email"]
+        user = s.query(User).filter(User.email == email).first()
+
+        if user == None:
+            return {"message": "User not found"}, 403
+
+        if not user.check_password(request.form["password"]):
+            return {"message": "Invalid password"}, 403
+
+        acces_token = create_access_token(
+            identity=user.id, additional_claims={"email": user.email, "id": user.id}
+        )
+        s.flush()
+        return {"access_tokern": acces_token, "message": "Success to Login user"}, 200
+    except Exception as e:
+        print(e)
+        s.rollback()
+        return {"message": "Failed to Login User"}, 500
