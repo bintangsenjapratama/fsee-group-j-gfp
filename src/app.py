7832a51e98b38ddfd5327cb01eaeb5d74630a4f4
LightKazuto <ilhamsidi62@gmail.com>
2024-07-31 07:36:39 +0700

    add jwt.token and jwt.revoked

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 
 from connectors.mysql_connectors import connection
 from controllers.users import users_routes
+from models.blocklist import BLOCKLIST
 from models.user import User
 
 
@@ -19,6 +20,16 @@ jwt = JWTManager(app)
 Session = sessionmaker(connection)
 
 app.register_blueprint(users_routes)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blocklist(jwt_header, jwt_payload):
+    return jwt_payload["jti"] in BLOCKLIST
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return {"description": "The token has been revoked.", "error": "token_revoked"}, 401
 
 
 @app.route("/")
