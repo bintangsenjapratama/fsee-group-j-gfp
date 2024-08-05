82cd596e7ca63232f786bf3722f4c9a5f5929105
LightKazuto <ilhamsidi62@gmail.com>
2024-08-05 20:35:21 +0700

    update: change ,env to railway

--- src/connectors/mysql_connectors.py
+++ src/connectors/mysql_connectors.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine
 import os
 
-username = os.getenv("DB_USERNAME")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_DATABASE")
+username = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+host = os.getenv("MYSQLHOST")
+database = os.getenv("MYSQLDATABASE")
+port = os.getenv("MYSQLPORT")
 
 # Connect to Database
 print("Connecting to MySQL Database")
 engine = create_engine(
-    f"mysql+mysqlconnector://{username}:{password}@{host}/{database}"
+    f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}"
 )
 print(engine)
 
