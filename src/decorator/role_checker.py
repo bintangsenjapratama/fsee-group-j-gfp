b584bd902bbfe0fc27445c159329c9d020fa6c49
LightKazuto <ilhamsidi62@gmail.com>
2024-08-05 20:40:00 +0700

    add: role_checker

--- src/decorator/role_checker.py
+++ src/decorator/role_checker.py
@@ -1,20 +1,21 @@
 from functools import wraps
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
+from flask import jsonify
 
 
-def role_required(role):
-    def decorator(func):
-        @wraps(func)
-        @jwt_required()  # tambahkan ini
+def role_required(required_role):
+    def decorator(fn):
+        @wraps(fn)
         def wrapper(*args, **kwargs):
-            jwt_identity = get_jwt_identity()
-            if isinstance(jwt_identity, dict):
-                if jwt_identity.get("role") == "admin" and role == "admin":
-                    return func(*args, **kwargs)
-                elif jwt_identity.get("role") == "user" and role == "user":
-                    return func(*args, **kwargs)
-            else:
-                return {"message": "Unauthorized"}, 403
+            claims = get_jwt()
+            roles = claims.get("seller", [])
+            if required_role not in roles:
+                return (
+                    jsonify(msg="Access forbidden: You don't have the required role"),
+                    403,
+                    print(claims),
+                )
+            return fn(*args, **kwargs)
 
         return wrapper
 
