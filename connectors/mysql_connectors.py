da53986c943c03ce5c18e377907853d03e9d8f89
LightKazuto <ilhamsidi62@gmail.com>
2024-08-20 20:14:45 +0700

    feat: change local api with railway

--- connectors/mysql_connectors.py
+++ connectors/mysql_connectors.py
@@ -1,29 +1,29 @@
 from sqlalchemy import create_engine
 import os
 
-# username = os.getenv("MYSQLUSER")
-# password = os.getenv("MYSQLPASSWORD")
-# host = os.getenv("MYSQLHOST")
-# database = os.getenv("MYSQLDATABASE")
-# port = os.getenv("MYSQLPORT")
-
-# print("Connecting to MySQL Database")
-# engine = create_engine(
-#     f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}"
-# )
-# print(engine)
-
-
-username = os.getenv("DB_USERNAME")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_DATABASE")
+username = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+host = os.getenv("MYSQLHOST")
+database = os.getenv("MYSQLDATABASE")
+port = os.getenv("MYSQLPORT")
 
 print("Connecting to MySQL Database")
 engine = create_engine(
-    f"mysql+mysqlconnector://{username}:{password}@{host}/{database}"
+    f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}"
 )
 print(engine)
+
+
+# username = os.getenv("DB_USERNAME")
+# password = os.getenv("DB_PASSWORD")
+# host = os.getenv("DB_HOST")
+# database = os.getenv("DB_DATABASE")
+
+# print("Connecting to MySQL Database")
+# engine = create_engine(
+#     f"mysql+mysqlconnector://{username}:{password}@{host}/{database}"
+# )
+# print(engine)
 
 connection = engine.connect()
 print("Success Connecting to MySQL User Database")
